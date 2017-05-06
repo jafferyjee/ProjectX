1945777a565a6a51e1c3c87c9a6eea31f4ab4103
jafferyjee <jafferyjee@hotmail.com>
2017-05-06 13:00:40 -0700

    Initialized from @Iftikhar_ali

---
+++ ProjectX.Repository/Models/ItemDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectX.Repository.Models
+{
+    public class ItemDetail
+    {
+        public string ItemFullCode { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public string ItemShortCode { get; set; }
+        public string LevelItemFullCode { get; set; }
+        public string SupplierCode { get; set; }
+        public string SubInvCode { get; set; }
+        public Nullable<decimal> PurchasePrice { get; set; }
+        public Nullable<decimal> SalePrice { get; set; }
+        public Nullable<System.DateTime> ArrivalDate { get; set; }
+        public string RefCode { get; set; }
+        public Nullable<decimal> TColumn { get; set; }
+        public Nullable<bool> TColumnByAmt { get; set; }
+        public Nullable<bool> IsGiftItem { get; set; }
+        public Nullable<bool> IsTransaction { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+
+        public string MasterCategoryFullCode { get; set; }
+        public string MasterCategoryName { get; set; }
+        public string CategoryFullCode { get; set; }
+        public string CategoryName { get; set; }
+        public string SubInvName { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
---
+++ ProjectX.Repository/Repositories/ItemRepository.cs
@@ -0,0 +1,93 @@
+﻿using ProjectX.Entities.Models;
+using Repository.Pattern.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectX.Repository.Models;
+
+namespace ProjectX.Repository.Repositories
+{
+    public static class ItemRepository
+    {
+        public static IEnumerable<Item> GetTransactionItems(this IRepositoryAsync<Item> repository) 
+        {
+            return repository
+                .Query()
+                .Include(i => i.SubInventory)
+                .Include(c => c.Contact)                
+                .OrderBy(q => q
+                    .OrderBy(i => i.ItemFullCode))                
+                .Select();
+        }
+
+        //public static IQueryable<Item> GetMainCategories(this IRepositoryAsync<Item> repository) 
+        //{
+        //    return repository
+        //        .Queryable()
+        //        .Where(i => i.IsTransaction == false && i.LevelItemFullCode == null)
+        //        .OrderBy(o => o.ItemFullCode)
+        //        .AsQueryable();
+        //}
+
+        //public static IQueryable<Item> GetSubCategories(this IRepositoryAsync<Item> repository) 
+        //{
+        //    return repository
+        //        .Queryable()
+        //        .Where(i => i.IsTransaction == false && i.LevelItemFullCode != null)
+        //        .OrderBy(o => o.ItemFullCode)
+        //        .AsQueryable();
+        //}
+
+        //public static IQueryable<Item> GetAllTransactionItems(this IRepositoryAsync<Item> repository) 
+        //{
+        //    return repository
+        //        .Queryable()
+        //        .Where(i => i.IsTransaction == true)
+        //        .OrderBy(i => i.ItemFullCode)
+        //        .AsQueryable();
+        //}
+
+        //public static IQueryable<ItemDetail> GetAllTransactionItemsDetail(this IRepositoryAsync<Item> repository) 
+        //{
+        //    var query =
+        //            from i in repository.GetRepository<Item>().Queryable()
+        //                where i.IsTransaction == true
+        //            join m in repository.GetRepository<Item>().Queryable() on i.LevelItemFullCode equals m.ItemFullCode
+        //            join c in repository.GetRepository<Item>().Queryable() on m.LevelItemFullCode equals c.ItemFullCode
+        //            orderby i.ItemFullCode 
+        //            select new ItemDetail { 
+        //                ItemFullCode = i.ItemFullCode,
+        //                ItemCode = i.ItemCode,
+        //                ItemName = i.ItemName,
+        //                ItemShortCode = i.ItemShortCode,
+        //                LevelItemFullCode = i.LevelItemFullCode,
+        //                SupplierCode = i.SupplierCode,
+        //                SubInvCode = i.SubInvCode,
+        //                PurchasePrice = i.PurchasePrice,
+        //                SalePrice = i.SalePrice,
+        //                ArrivalDate = i.ArrivalDate,
+        //                RefCode = i.RefCode,
+        //                TColumn = i.TColumn,
+        //                TColumnByAmt = i.TColumnByAmt,
+        //                IsGiftItem = i.IsGiftItem,
+        //                IsTransaction = i.IsTransaction,
+        //                IsActive = i.IsActive,
+        //                CreatedBy = i.CreatedBy,
+        //                CreatedDate = i.CreatedDate,
+        //                ModifiedBy = i.ModifiedBy,
+        //                ModifiedDate = i.ModifiedDate,
+        //                SupplierName = i.Contact.ContactName,
+        //                SubInvName = i.SubInventory.SubInvName,
+        //                MasterCategoryFullCode = m.ItemFullCode,
+        //                MasterCategoryName = m.ItemName,
+        //                CategoryFullCode = c.ItemFullCode,
+        //                CategoryName = c.ItemName
+        //            };
+
+        //    return query.AsQueryable();
+        //}
+
+    }
+}
