2c4e017fd2f14e0231615ac3899d2e9fd296ba67
jafferyjee <jafferyjee@hotmail.com>
2017-05-20 08:22:40 -0700

    Initialized @ Iftikhar_ali

---
+++ ProjectX.Core/Caching/Provider/NullCache.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ProjectX.Core.Caching.Provider
+{
+    public class NullCache : ICacheProvider
+    {
+
+        #region ICacheProvider Members
+
+        public object this[string key]
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public object Get(string itemKey)
+        {
+            return null;
+        }
+
+        public void ClearCache()
+        {
+        }
+
+        public void Remove(string itemKey)
+        {
+        }
+
+        public void Insert(string keyString, object value, int cacheDurationInSeconds, System.Web.Caching.CacheItemPriority priority)
+        {
+        }
+
+        public System.Collections.IDictionaryEnumerator GetEnumerator()
+        {
+            return new System.Collections.Hashtable().GetEnumerator();
+        }
+
+        public int GetCount()
+        {
+            return 0;
+        }
+
+        #endregion
+
+    }
+}
