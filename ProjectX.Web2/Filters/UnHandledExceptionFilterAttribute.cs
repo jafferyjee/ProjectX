d797e5911b80d53726f6c736d6a84e4aae2e9ce0
jafferyjee <jafferyjee@hotmail.com>
2017-05-20 09:57:46 -0700

    committed @ iftikhar_ali

---
+++ ProjectX.Web2/Filters/UnHandledExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using ProjectX.Web.Utility;
+
+namespace ProjectX.Web.Filters
+{
+    public class UnHandledExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override async System.Threading.Tasks.Task OnExceptionAsync(HttpActionExecutedContext actionExecutedContext, System.Threading.CancellationToken cancellationToken)
+        {
+            await LogException(actionExecutedContext.Exception);
+        }
+
+        private System.Threading.Tasks.Task LogException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
